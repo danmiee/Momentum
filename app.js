075a7d42ac2e2ab7dc75971f544746ead7f59455
danmiee <dan-mi@kakao.com>
2022-10-06 20:51:17 +0900

    3.8 css in javascript part three

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 handleTitleClick = () => {
-  const clickedClass = "clicked sexy-font";
-  if(h1.className === "clickedClass") {
-    h1.className = "";
-  } else {
-    h1.className = "clickedClass";
-  }
+  /*
+  className : class명 교체
+  classList : 특정 class명만 수정 가능
+  */
+ 
+ // toggle은 아래 5줄의 코드와 같은 역할을 함
+ h1.classList.toggle("clicked");
+  // const clickedClass = "clicked";
+  // if(h1.classList.contains(clickedClass)) {
+  //   h1.classList.remove(clickedClass);
+  // } else {
+  //   h1.classList.add(clickedClass);
+  // }
 }
 
 h1.addEventListener("click", handleTitleClick);
