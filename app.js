6a0db1996e42a99523dfed55e057f342c6c801ae
danmiee <dan-mi@kakao.com>
2022-10-06 20:18:35 +0900

    3.5 more events

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
-const title = document.querySelector("div.hello:first-child h1");
-
-console.dir(title);
+const h1 = document.querySelector("div.hello:first-child h1");
 
 handleTitleClick = () => {
-  title.style.color = "blue";
+  h1.style.color = "blue";
 }
 
 handleMouseEnter = () => {
-  title.innerText = "Mouse is here!";
+  h1.innerText = "Mouse is here!";
 }
 
 handleMouseLeave = () => {
-  title.innerText = "Mouse is gone!";
+  h1.innerText = "Mouse is gone!";
 }
 
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+handleWindowResize = () => {
+  document.body.style.backgroundColor = "tomato";
+}
+
+handleWindowCopy = () => {
+  alert("copier!");
+}
+
+handleWindowOffline = () => {
+  alert("SOS no WIFI");
+}
+
+handleWindowOnline = () => {
+  alert("Allllllll Goooooooood");
+}
+
+h1.addEventListener("click", handleTitleClick);
+// (같은효과) h1.onclick = handleTitleClick;
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+/*
+addEventListener      이벤트 생성
+.removeEventListener  이벤트 제거
+onclick, onmouseenter 등으로 작성했을 때는 remove함수 없음
+*/
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
