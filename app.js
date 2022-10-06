87eec9fc9bed9cafe0165c794a5c53a869d367b8
danmiee <dan-mi@kakao.com>
2022-10-06 19:59:45 +0900

    3.4 events part two

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-const title = document.querySelector("div.hello h1");
+const title = document.querySelector("div.hello:first-child h1");
+
+console.dir(title);
 
 handleTitleClick = () => {
-  console.log("title was clicked!");
+  title.style.color = "blue";
+}
+
+handleMouseEnter = () => {
+  title.innerText = "Mouse is here!";
+}
+
+handleMouseLeave = () => {
+  title.innerText = "Mouse is gone!";
 }
 
 title.addEventListener("click", handleTitleClick);
-
-handleTitleClick()
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
