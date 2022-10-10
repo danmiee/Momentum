4c81a4c97d09e8cc16f9fc5a414746911cc907a0
dan-mi <dan-mi@kakao.com>
2022-10-10 17:29:42 +0900

    4.1 Form Submission

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+// const loginForm = document.getElementById("login-form");
+// const loginInput = loginForm.querySelector("input");
+// const loginButton = loginForm.querySelector("button");
+// loginForm이 html의 요소라서 그 안에서 input과 button을 찾을 수 있음
 
-handleTitleClick = () => {
-  /*
-  className : class명 교체
-  classList : 특정 class명만 수정 가능
-  */
- 
- // toggle은 아래 5줄의 코드와 같은 역할을 함
- h1.classList.toggle("clicked");
-  // const clickedClass = "clicked";
-  // if(h1.classList.contains(clickedClass)) {
-  //   h1.classList.remove(clickedClass);
-  // } else {
-  //   h1.classList.add(clickedClass);
-  // }
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+/*
+onLoginBtnClick = () => {
+  const username = loginInput.value;
+  // 유효성검사 연습
+    if (username === "") {
+      alert("Please write your name");
+    } else if(username.length>15) {
+      alert ("Your name is too long.")
+    }
+    // html에서 input 속성으로 제한 가능
+    // (단, input이 form의 자식요소일 때만 적용됨)
+  
+  console.log(username);
 }
 
-h1.addEventListener("click", handleTitleClick);
+loginButton.addEventListener("click", onLoginBtnClick);
+// button이 아닌 submit타입 input으로 작성 가능
+*/
