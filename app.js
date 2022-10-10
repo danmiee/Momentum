a7a618b5d67d23b1552b8003a7ff2f87ec0b37a9
dan-mi <dan-mi@kakao.com>
2022-10-10 19:30:45 +0900

    4.6 Loading Username

--- app.js
+++ app.js
@@ -3,14 +3,30 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+// 반복되는 string을 대문자 변수로 저장해두면 컴파일러가 오타 점검해줌
 
 onLoginSubmit = (event) => {
   event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username",username);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+// 중복되는 내용 함수로 만들어주기
+paintGreetings = (username) => {
   greeting.innerText = `Hello, ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  // show the greetings
+  paintGreetings(savedUsername);
+}
