7561c5af988e7f7548f196238d9f9dd22acc27f4
dan-mi <dan-mi@kakao.com>
2022-10-10 19:04:45 +0900

    4.5 Saving Username

--- app.js
+++ app.js
@@ -3,19 +3,14 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
-// string만 포함된 변수는 대문자 표기
 
 onLoginSubmit = (event) => {
   event.preventDefault();
-  // 로그인폼 숨기기
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  // 유저이름 변수로 저장
   const username = loginInput.value;
-  // 유저이름 h1에 넣기
-  // greeting.innerText = "Hello, " + username;
+  localStorage.setItem("username",username);
   greeting.innerText = `Hello, ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-
 
 loginForm.addEventListener("submit", onLoginSubmit);
