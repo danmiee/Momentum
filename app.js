791a1697d448b13af5da199507bd612f26e8a307
dan-mi <dan-mi@kakao.com>
2022-10-10 18:03:09 +0900

    4.3 Events part Two

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 onLoginSubmit = (event) => {
   event.preventDefault();
   // 첫번째 argument로 발생된 event에 대한 정보 받기 & 브라우저가 기본 동작(새로고침) 실행하지 못하게 막기
   console.log(event);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+handleLinkClick = (event) => {
+  event.preventDefault();
+  console.dir(event);
+}
 
-// 자바스크립트는 정보를 가진 채로 함수를 실행시킴
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
+
+// addEventListener 안의 함수는 직접 실행X > 브라우저가 동작 인식 후 알아서 실행
+// 이벤트에 대한 정보(the first element - information about the event that just happened)를 가진 채로 함수를 실행시킴
