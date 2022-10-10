83fc1f5ffd9873b1b3f76d5fb27dacdf16d22224
dan-mi <dan-mi@kakao.com>
2022-10-10 21:09:59 +0900

    5.3 Recap

--- js/clock.js
+++ js/clock.js
@@ -1,11 +1,14 @@
 const clock = document.querySelector("#clock")
 
 getClock = () => {
+    // Date object : 현재 일시 호출 가능
     const date = new Date();
+    // 숫자인 date.get____을 String으로 감싸서 문자열로 만들기
     // padStart(자리수,공란채울 텍스트)
     const hours = String(date.getHours()).padStart(2,"0");
     const minutes = String(date.getMinutes()).padStart(2,"0");
     const seconds = String(date.getSeconds()).padStart(2,"0");
+    // clock 위치에 텍스트로 넣어주기
     clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
