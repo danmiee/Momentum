2cec12fa734f5a9dce199b20c8d91c94ada3c90f
dan-mi <dan-mi@kakao.com>
2022-10-10 21:03:59 +0900

    5.2 PadStart

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,11 @@ const clock = document.querySelector("#clock")
 
 getClock = () => {
     const date = new Date();
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    // padStart(자리수,공란채울 텍스트)
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 // 시계 즉시호출
