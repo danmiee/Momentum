668cc628ce429c6b5c84e950508344cd42542e1d
danmiee <dan-mi@kakao.com>
2022-10-12 19:50:18 +0900

    7.1 Adding ToDos

--- js/todo.js
+++ js/todo.js
@@ -3,11 +3,19 @@ const toDoInput = document.querySelector("#todo-form input");
 // const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+paintToDo = (newTodo) => {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newTodo;
+  toDoList.appendChild(li);
+}
+
 handletoDoSubmit = (event) => {
   event.preventDefault();
   const newTodo = toDoInput.value;   // toDoInput 값을 새로운 변수에 복사
   toDoInput.value = "";
-
+  paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handletoDoSubmit);
