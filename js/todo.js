a19411eef4ce3dcfc85b35a2cef0501cd334d998
danmiee <dan-mi@kakao.com>
2022-10-12 20:03:20 +0900

    7.2 Deleting To Dos

--- js/todo.js
+++ js/todo.js
@@ -3,11 +3,23 @@ const toDoInput = document.querySelector("#todo-form input");
 // const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+deleteToDo = (event) => {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 paintToDo = (newTodo) => {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
+  const button = document.createElement("button");
+
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+
+  // append는 항상 마지막에
+  li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
