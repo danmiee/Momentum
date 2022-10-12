d65424e4bacd3b87c885ecff68dd3f9ee81bd606
danmiee <dan-mi@kakao.com>
2022-10-12 20:31:03 +0900

    7.4 Loading ToDos part One

--- js/todo.js
+++ js/todo.js
@@ -3,20 +3,24 @@ const toDoInput = document.querySelector("#todo-form input");
 // const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
 const toDos = [];
 
-saveToDos = (event) => {
+// JSON.parse : 배열로 반환
+
+function saveToDos() {
   // localStage는 array 저장불가, text만 저장가능
   // JSON.stringify : object, array, 어떤 코드든 String으로 만들어줌
-  localStorage.setItem("todos", JSON.stringify(toDos));
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
-deleteToDo = (event) => {
+function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
 }
 
-paintToDo = (newTodo) => {
+function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
   span.innerText = newTodo;
@@ -31,7 +35,7 @@ paintToDo = (newTodo) => {
   toDoList.appendChild(li);
 }
 
-handletoDoSubmit = (event) => {
+function handletoDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;   // toDoInput 값을 새로운 변수에 복사
   toDoInput.value = "";
@@ -41,3 +45,19 @@ handletoDoSubmit = (event) => {
 }
 
 toDoForm.addEventListener("submit", handletoDoSubmit);
+
+function sayHello(item) {
+  console.log("this is the turn of", item);
+}
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+console.log(savedToDos);
+
+if(savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  console.log(parsedToDos);
+  
+  // 함수를 따로 만들지 않고 화살표 함수 사용
+    // parsedToDos에 있는 각각의 item에 대해 => 뒤 내용을 실행
+  parsedToDos.forEach((item) => console.log("this is the turn of ", item));
+}
