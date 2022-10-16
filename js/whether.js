5cbc6d23672ad926e10b09ca02dc5209eff955b6
dan-mi <dan-mi@kakao.com>
2022-10-16 19:30:14 +0900

    8.1 Weather API

--- js/whether.js
+++ js/whether.js
@@ -1,10 +1,27 @@
+const API_KEY = "c9b1e2be6af63ff8c360531f39f8a440";
+
 function onGeoOK(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live it", lat, lng);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main}, ${data.main.temp}`;
+        /* Error
+            weather.js:12 Uncaught (in promise) TypeError: Cannot set properties of null (setting 'innerText')
+            (solve)
+            line 10~11 #weather : div id "weather"
+            line 12~13 innerText = url path check
+        */
+});
 }
+
 function onGeoError(){
-    alert("Can't find you. No weather for you.")
+    alert("Can't find you. No weather for you.");
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOK,onGeoError);
